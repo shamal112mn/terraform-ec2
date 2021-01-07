1dc3d5e7887a926c45e6362299ffcc41ee3393db
shamal112mn <azatmurataliev@gmail.com>
2021-01-07 12:41:58 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ resource "aws_key_pair" "my-key" {
 resource "aws_instance" "web" {
   key_name = aws_key_pair.my-key.key_name
   instance_type = var.instance_type
-  ami           = var.ami_id
+  ami           = data.aws_ami.my_ami.id
   security_groups = [aws_security_group.web.id]
 
   subnet_id     = tolist(data.aws_subnet_ids.my-subnets.ids)[0]
@@ -19,3 +19,18 @@ resource "aws_instance" "web" {
     Name = "${var.environment}-instance"
   }
 }
+
+data "aws_ami" "my_ami" {
+		most_recent = true
+		filter {
+		name = "name"
+		values = ["dev-66752d1b-320a-4c12-bf42-233eb7f2c331*"]
+		}
+		filter {
+		name = "virtualization-type"
+		values = ["hvm"]
+		}
+		 owners           = ["self"] 
+	}
+
+
